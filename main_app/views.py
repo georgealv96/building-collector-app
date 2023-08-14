d821a1131ab3c6367bd22ad26087d6074f9ef8ec
georgealv96 <george1996alvarado@gmail.com>
2023-08-14 10:48:52 -0600

    creating the index page for the finches

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+finches = [
+    {'type': "Cassin's Finch", 'color': 'red'},
+    {'type': 'Common Redpoll', 'color': 'red'},
+    {'type': 'Goldfinches', 'color': 'yellow'},
+    {'type': 'Greenfinches', 'color': 'green'},
+    {'type': 'Blue Grosbeak', 'color': 'blue'},
+    {'type': 'White Winged Snow Finch', 'color': 'white'}
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })

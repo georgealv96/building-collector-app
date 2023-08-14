f488611cea0293f550d3cfcddc6de65a836c2da8
georgealv96 <george1996alvarado@gmail.com>
2023-08-14 14:26:06 -0600

    creating a Building model for the database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length=100)
+    opening_year = models.IntegerField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    height_in_feet = models.FloatField()
+    

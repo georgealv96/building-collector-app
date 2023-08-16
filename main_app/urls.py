380a0146c48c29c37860843d7fb8c0f5b4ddbd01
georgealv96 <george1996alvarado@gmail.com>
2023-08-16 13:38:20 -0600

    implementing the paths for the templates of the new model

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('buildings/<int:pk>/update/', views.BuildingUpdate.as_view(), name='buildings_update'),
     path('buildings/<int:pk>/delete/', views.BuildingDelete.as_view(), name='buildings_delete'),
     path('buildings/<int:building_id>/add_visit/', views.add_visit, name='add_visit'),
+    path('references/', views.ReferenceList.as_view(), name='references_index'),
+    path('references/create/', views.ReferenceCreate.as_view(), name='references_create'),
 ]
